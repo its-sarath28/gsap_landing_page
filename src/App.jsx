099e6ea40fb0006e6ade5d732a81c5a6b0b8c125
its-sarath28 <sarathrs220@gmail.com>
2025-09-05 16:56:42 +0530

    Implemented Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 
+import Navbar from "./components/Navbar";
+
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
 };
 
 export default App;
